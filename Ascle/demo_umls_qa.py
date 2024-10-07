840a244579355899d3c7474b6523445dc83217bb
Mónica Pina Navarro <82449538+monicapina@users.noreply.github.com>
2024-10-07 17:03:40 +0900

    Update demo_umls_qa.py

--- Ascle/demo_umls_qa.py
+++ Ascle/demo_umls_qa.py
@@ -1,17 +1,16 @@
 # Import the necessary classes
-from medical_qa import UmlsQA  # Import MedicalAssistantInterface class
+from medical_qa import UmlsQA
 
 # Code that will be called from the main
 if __name__ == "__main__":
-    # Initialize the ModelManager, which loads the API keys from the .env file
     
-    # Initialize the UmlsQA using the model manager to handle API keys
+    # Initialize the UmlsQA 
     assistant = UmlsQA(model_name="gpt-3.5-turbo", api_key="xxxx")  
     
     # Define the medical question in a variable
     question = "How does smoking affect lung function?"
     
-    # Ask the medical question and get the response from the assistant
+    # Ask the medical question and get the response
     response = assistant.ask_medical_question(question)  
     
     # Print the response in English
