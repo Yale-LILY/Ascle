d9211d48ef5141e8950a32c2030f45d6b5d5e858
Peng Wei <pengwei715@gmail.com>
2024-10-24 22:04:01 -0700

    adding the base class

---
+++ ascle-refactor/core/base.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
+
+# ------------------------------------------------------------------------------
+#   Copyright (C) 2024 Li-lab
+#   @Filename: base.py
+#   @Author: Peng Wei
+#   @Date: 2024-10-24
+#   @Email: weipeng0715@gmail.com
+#   @Description: 
+# ------------------------------------------------------------------------------
+from abc import ABC, abstractmethod
+from typing import List, Optional, Union
+from dataclasses import dataclass
+
+@dataclass
+class ModelConfig:
+    name: str
+    device: str = "cpu"
+    max_length: int = 512
+    batch_size: int = 32
+
+class BaseProcessor(ABC):
+    def __init__(self, config: ModelConfig):
+        self.config = config
+        self._model = None
+        self._tokenizer = None
+
+    @abstractmethod
+    def load_model(self):
+        pass
+
+    @abstractmethod
+    def process(self, text: Union[str, List[str]]):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cleanup()
+
+    def cleanup(self):
+        if hasattr(self, '_model'):
+            del self._model
